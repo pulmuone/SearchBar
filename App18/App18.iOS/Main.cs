e3dafe1c3a7dcb1dee3ea5d76cbeef0fcdb7ddc5
지현명 <gwise@naver.com>
2019-03-13 08:58:07 +0900

    1

--- App18/App18.iOS/Main.cs
+++ App18/App18.iOS/Main.cs
@@ -14,6 +14,7 @@ namespace App18.iOS
         {
 
             DevExpress.Mobile.Forms.Init();
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
